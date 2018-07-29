dd5569f0006223a10b7407704fec98c3754fd861
spirit <catchspirit37@gmail.com>
2018-07-29 11:18:04 +0800

    websocket port

--- config/diffConfig.js
+++ config/diffConfig.js
@@ -4,8 +4,12 @@
 const diffConfig = {
     db:{
         username:"root",
-        password:""
-    }
+        password:"spirit12#"
+    },
+    port:"3000",
+    wsPort:"3001",
+    ip:"192.168.8.101"
+
 
 }
 
