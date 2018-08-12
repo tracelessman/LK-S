62cfe4b939ae3cfa14aec46ecd5ab2918aa99199
spirit <catchspirit37@gmail.com>
2018-08-12 11:34:20 +0800

    fix

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+const path = require('path')
+const rootPath = path.resolve(__dirname,'')
+const localFolder = path.resolve(rootPath,'local')
+const coverageFolder = path.resolve(localFolder,'coverage')
+const fse = require('fs-extra')
+fse.ensureDirSync(coverageFolder)
+
+
+module.exports = {
+    bail: true,
+    collectCoverage:true,
+    coverageDirectory:coverageFolder
+};
