63f24df0008b441201dff61221d4fe1b7d19c2a7
spirit <catchspirit37@gmail.com>
2018-08-14 13:53:59 +0800

    z

--- bin/remoteDeploy.js
+++ bin/remoteDeploy.js
@@ -4,6 +4,10 @@ const rootPath = path.resolve(__dirname,'../')
 const childProcess = require('child_process')
 
 const gitCmd = require('simple-git')(rootPath)
+console.log('noew')
+
 const result = childProcess.execSync(`git pull`).toString()
+console.log('good')
+
 console.log(result)
 
