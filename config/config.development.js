9218ad67bdfd48a54cec4eb36887a80c0cdd0c0e
spirit <catchspirit37@gmail.com>
2018-08-14 14:34:43 +0800

    z

--- config/config.development.js
+++ config/config.development.js
@@ -2,7 +2,7 @@ const childProcess = require('child_process')
 const os = require('os')
 const assert = require('assert')
 
-assert(os.platform()!=='darwin')
+assert(os.platform() === 'darwin')
 const cmd = 'ipconfig getifaddr en0'
 const ip = childProcess.execSync(cmd).toString().trim()
 
