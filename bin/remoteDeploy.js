c242c4c30af88eef1ef5db98a492b4d7afc7f182
spirit <catchspirit37@gmail.com>
2018-08-14 13:48:16 +0800

    z

--- bin/remoteDeploy.js
+++ bin/remoteDeploy.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
+const path = require('path')
+const rootPath = path.resolve(__dirname,'../')
 
-const gitCmd = require('simple-git')
+
+const gitCmd = require('simple-git')(rootPath)
 gitCmd.pull(()=>{
     console.log(arguments)
 
