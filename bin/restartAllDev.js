b27042c8ef33720fbacdf17c232b7cee770fc2a1
spirit <catchspirit37@gmail.com>
2018-09-19 11:34:29 +0800

    z - Wed Sep 19 2018 11:34:29 GMT+0800 (China Standard Time)

--- bin/restartAllDev.js
+++ bin/restartAllDev.js
@@ -16,39 +16,43 @@ const {SSHUtil} = require('@ys/collection')
 const {execCommand: execCommandCollection} = SSHUtil
 const _ = require('lodash')
 const debug = require('debug')('restartAllDev')
-
 const config = require(path.resolve(rootPath, 'config'))
-;(async () => {
-  const option = {
-    host: config.txServerIp,
-    username: config.sshInfo.username,
-    password: config.sshInfo.password
-  }
-  debug({option})
-  await ssh.connect(option)
-  const folderName = 'testing'
-  const basePath = '/opt'
-  const testingFolder = path.resolve(basePath, folderName)
-  const projectFolder = path.resolve(testingFolder, getRepoName(config.repo))
-  let cmd = `
+
+start()
+
+function start () {
+  (async () => {
+    const option = {
+      host: config.txServerIp,
+      username: config.sshInfo.username,
+      password: config.sshInfo.password
+    }
+    debug({option})
+    await ssh.connect(option)
+    const folderName = 'testing'
+    const basePath = '/opt'
+    const testingFolder = path.resolve(basePath, folderName)
+    const projectFolder = path.resolve(testingFolder, getRepoName(config.repo))
+    let cmd = `
   git reset --hard;
   git pull;
   npm install --production;
   `
-  await execCommand(cmd, projectFolder)
-  info(['already pulled and installed'])
-  await execCommand('node bin/serverScript', projectFolder)
-  info(['serverScript finished'])
+    await execCommand(cmd, projectFolder)
+    info(['already pulled and installed'])
+    await execCommand('node bin/serverScript', projectFolder)
+    info(['serverScript finished'])
 
-  cmd = `
+    cmd = `
   npm run runAllNuxt;
   `
-  await execCommand(cmd, projectFolder)
-  ssh.dispose()
-  info(['testing deployment updated'])
-})().catch(err => {
-  error([err])
-})
+    await execCommand(cmd, projectFolder)
+    ssh.dispose()
+    info(['testing deployment updated'])
+  })().catch(err => {
+    error([err])
+  })
+}
 
 function execCommand (cmd, cwd, option = {}) {
   _.merge(option, {
