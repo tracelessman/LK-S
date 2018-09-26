486b0ad29e8eac78cd5a7f52b84892cd93bbd1a1
spirit <catchspirit37@gmail.com>
2018-09-26 14:08:45 +0800

    z - Wed Sep 26 2018 14:08:45 GMT+0800 (China Standard Time)

--- frontend/util/netUtil.js
+++ frontend/util/netUtil.js
@@ -1,64 +1,62 @@
-//in vue context
+// in vue context
 
 const axios = require('axios')
 const shareUtil = require('../../share/util')
-const frontState = require("../../state/frontState")
+const frontState = require('../../state/frontState')
 
 const netUtil = {
-    httpPost(option){
-        const {url,param,successCb,failCb} = option
-        axios({
-                headers: { 'content-type': 'application/json' },
-                method: 'post',
-                url: url,
-                data: shareUtil.objectify(param)
-            })
-            .then( (response)=>{
+  httpPost (option) {
+    const {url, param, successCb, failCb} = option
+    axios({
+      headers: { 'content-type': 'application/json' },
+      method: 'post',
+      url,
+      data: shareUtil.objectify(param)
+    })
+      .then((response) => {
+        const {data} = response
+        const {error, content, errorMsg, isExpired} = data
+        if (error || errorMsg) {
+          if (shareUtil.runFunc(failCb, error)) {
+            console.log(error)
+          } else {
+            processError(error, errorMsg)
+          }
+        } else {
+          if (isExpired) {
+            location = '/login'
+          } else {
+            shareUtil.runFunc(successCb, content)
+          }
+        }
+      })
+      .catch((error) => {
+        console.log(error)
 
-                const {data} = response
-                const {error,content,errorMsg,isExpired} = data
-                if(error || errorMsg){
-                    if(shareUtil.runFunc(failCb,error)){
-                        console.log(error)
-                    }else{
-                        processError(error,errorMsg)
-                    }
-                }else{
-                    if(isExpired){
-                        location = '/login'
-                    }else{
-                        shareUtil.runFunc(successCb,content)
-                    }
-                }
-            })
-            .catch((error)=>{
-                console.log(error
+        axios.get('api/test/ping', {
+        }).then(() => {
+          processError(error)
+        }).catch(error => {
+          console.log(error)
 
-                axios.get('api/test/ping',{
-                }).then(()=>{
-                    processError(error)
-                }).catch(error=>{
-                    console.log(error)
-
-                    frontState.rootView.$Message.error({
-                        content: `网络已断开,请检查您的网络连接!`,
-                        duration: 5
-                    })
-                })
-            });
-
-    },
+          frontState.rootView.$Message.error({
+            content: `网络已断开,请检查您的网络连接!`,
+            duration: 5
+          })
+        })
+      })
+  }
 
 }
 
-function processError(err,errorMsg){
-    if(!errorMsg){
-        errorMsg = '操作失败!'
-    }
-    frontState.rootView.$Message.error({
-        duration:4,
-        content:errorMsg
-    })
+function processError (err, errorMsg) {
+  if (!errorMsg) {
+    errorMsg = '操作失败!'
+  }
+  frontState.rootView.$Message.error({
+    duration: 4,
+    content: errorMsg
+  })
 }
 
 module.exports = netUtil
