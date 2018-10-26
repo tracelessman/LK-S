68ae8afdae15f2e45668e6147ab6b14f94bbf01c
spirit <catchspirit37@gmail.com>
2018-10-26 13:39:13 +0800

    z - Fri Oct 26 2018 13:39:13 GMT+0800 (CST)

--- pages/app.vue
+++ pages/app.vue
@@ -1,29 +1,22 @@
 <template>
     <div>
-        <div style="text-align: center;font-size: 3em" v-if="isIphone">
-            <div>
-                <img src="/img/app2.png" style="width:40%;margin:100px;border-radius: 15px;margin-top: 100px"/>
-            </div>
-            <div>
-                <i-button @click="click" type="primary" shape="circle" size="large" icon="ios-cloud-download-outline" style="width:50%;height: 120px;font-size: 1.3em;border-radius: 80px">下载LK</i-button>
-            </div>
-            <div style="margin-top: 30px;color:#2d8cf0">
-                {{this.info}}
-            </div>
-        </div>
-        <div  style="text-align: center;font-size: 1.2em;" v-else>
-            <img id='barcode'
-                 src="https://api.qrserver.com/v1/create-qr-code/?data=<%=url%>/app&amp;size=200x200"
-                 alt=""
-                 title="HELLO"
-                 width="100px"
-                 height="100px"
-                 style="margin-top: 200px"
-            />
-            <div style="margin-top: 30px;color:#2d8cf0">
-                {{this.info}}
-            </div>
-        </div>
+        <Tabs type="card" style="margin: 20px">
+            <TabPane label="苹果">
+                <div style="text-align: center;font-size: 1em" >
+                    <div>
+                        <img src="/img/app2.png" style="width:40%;margin:100px;border-radius: 15px;margin-top: 100px"/>
+                    </div>
+                    <div>
+                        <i-button @click="click" type="primary" shape="circle" size="large" icon="ios-cloud-download-outline" style="width:50%;height: 60px;font-size: 1.3em;border-radius: 80px">下载LK</i-button>
+                    </div>
+                    <div style="margin-top: 30px;color:#2d8cf0">
+                        {{this.info}}
+                    </div>
+                </div>
+            </TabPane>
+            <TabPane label="安卓">标签二的内容</TabPane>
+        </Tabs>
+
     </div>
 </template>
 
@@ -33,7 +26,6 @@
     data() {
       return  {
         info:"请用您的iphone或ipad加载本网页或扫描页面上的二维码",
-        isIphone:true,
         url:"itms-services://?action=download-manifest&url=https://raw.githubusercontent.com/tracelessman/LK-M/manifest/manifest.plist"
       }
     },
