4ed323d9960009eb27bacf280915b791c78d402b
spirit <catchspirit37@gmail.com>
2018-10-26 14:02:34 +0800

    z - Fri Oct 26 2018 14:02:34 GMT+0800 (CST)

--- pages/app.vue
+++ pages/app.vue
@@ -7,14 +7,25 @@
                         <img src="/img/app2.png" style="width:40%;margin:100px;border-radius: 15px;margin-top: 100px"/>
                     </div>
                     <div>
-                        <i-button @click="click" type="primary" shape="circle" size="large" icon="ios-cloud-download-outline" style="width:50%;height: 60px;font-size: 1.3em;border-radius: 80px">下载LK</i-button>
+                        <i-button @click="clickIOS" type="primary" shape="circle" size="large" icon="ios-cloud-download-outline" style="width:50%;height: 60px;font-size: 1.3em;border-radius: 80px">下载LK</i-button>
                     </div>
                     <div style="margin-top: 30px;color:#2d8cf0">
                         {{this.info}}
                     </div>
                 </div>
             </TabPane>
-            <TabPane label="安卓">标签二的内容</TabPane>
+            <TabPane label="安卓">
+                <div style="text-align: center;font-size: 1em" >
+                <div>
+                    <img src="/img/app2.png" style="width:40%;margin:100px;border-radius: 15px;margin-top: 100px"/>
+                </div>
+                <div>
+                    <i-button @click="clickAndroid" type="primary" shape="circle" size="large" icon="ios-cloud-download-outline" style="width:50%;height: 60px;font-size: 1.3em;border-radius: 80px">下载LK</i-button>
+                </div>
+                <div style="margin-top: 30px;color:#2d8cf0">
+                    {{this.info}}
+                </div>
+            </div></TabPane>
         </Tabs>
 
     </div>
@@ -26,12 +37,15 @@
     data() {
       return  {
         info:"请用您的iphone或ipad加载本网页或扫描页面上的二维码",
-        url:"itms-services://?action=download-manifest&url=https://raw.githubusercontent.com/tracelessman/LK-M/manifest/manifest.plist"
+        url:"itms-services://?action=download-manifest&url=https://raw.githubusercontent.com/tracelessman/LK-M/master/ios/manifest.plist"
       }
     },
     methods: {
-      click(){
+      clickIOS(){
         location.href = this.url
+      },
+      clickAndroid() {
+        location.href = 'http://192.144.200.234:3000/public/android/LK.apk'
       }
     },
     mounted(){
