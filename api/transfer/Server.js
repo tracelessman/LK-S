e65033defa242dc1b79ab9508824d27be2cea786
randomyige <randomyige@gmail.com>
2018-10-31 16:37:27 +0800

    tmp

--- api/transfer/Server.js
+++ api/transfer/Server.js
@@ -524,7 +524,7 @@ let LKServer = {
             }else{
                f = await Message.asyGetLocalFlowbyParentMsgId(msgId,msg.header.uid,msg.header.did);
             }
-            if(!f){
+            if(f){
                 nCkDiff = true;
             }
         }
