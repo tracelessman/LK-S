00ad6799d82b76e0b02dd7be405b4cad3bee57ad
spirit <catchspirit37@gmail.com>
2019-04-03 14:16:09 +0800

    pool create view after model synced

--- api/store/pool.js
+++ api/store/pool.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql2')
 const config = require('../../config')
+const {ormModelPromise} = require('./ormModel')
 
 const option = {
     connectionLimit : 5,
@@ -9,5 +10,18 @@ const option = {
 }
 const pool  = mysql.createPool(option);
 
+createView()
+
+async function createView() {
+  const ormModel = await ormModelPromise
+  const psAry = []
+  for(let ele in ormModel) {
+    const {modelSequelized} = ormModel[ele]
+
+    psAry.push(modelSequelized.sync())
+  }
+  await Promise.all(psAry)
+  // drop and create view
+}
 
 module.exports = pool
