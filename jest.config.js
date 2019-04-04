cbd35a661a62019e42f487e019700a89684c4565
gitpanyu <1224721916@qq.com>
2019-04-04 10:23:06 +0800

    pan

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,13 @@
 const path = require('path')
-const rootPath = path.resolve(__dirname,'')
-const localFolder = path.resolve(rootPath,'local')
-const coverageFolder = path.resolve(localFolder,'coverage')
+const rootPath = path.resolve(__dirname, '')
+const localFolder = path.resolve(rootPath, 'local')
+const coverageFolder = path.resolve(localFolder, 'coverage')
 const fse = require('fs-extra')
 fse.ensureDirSync(localFolder)
 fse.ensureDirSync(coverageFolder)
 
-
 module.exports = {
-    bail: true,
-    collectCoverage:true,
-    coverageDirectory:coverageFolder
+  bail: true,
+  collectCoverage: true,
+  coverageDirectory: coverageFolder
 }
